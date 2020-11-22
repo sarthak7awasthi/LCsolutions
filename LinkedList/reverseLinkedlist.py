056d79bb598293d488769b280563837b739a4fd4
sarthak7awasthi <61361866+sarthak7awasthi@users.noreply.github.com>
2020-11-21 19:48:48 -0500

    rll

---
+++ LinkedList/reverseLinkedlist.py
@@ -0,0 +1,11 @@
+def reverseList(self, head):
+    prev = None
+    curr = head
+
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    
+    return prev
