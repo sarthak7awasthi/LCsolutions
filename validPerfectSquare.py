5bbe820fd660ca94ccaff7ee1afb577f4ee90c61
sarthak7awasthi <61361866+sarthak7awasthi@users.noreply.github.com>
2021-02-13 23:47:15 -0500

    vps

---
+++ validPerfectSquare.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        start=0
+        end=num
+        while start<=end:
+            mid=start + (end-start) //2
+            if mid*mid==num:
+                return True
+            elif mid*mid>num:
+                end=mid-1
+            else:
+                start=mid+1
+        return False
