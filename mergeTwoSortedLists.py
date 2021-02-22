776b364074c692838e29d83b823fe927a0f05381
sarthak7awasthi <61361866+sarthak7awasthi@users.noreply.github.com>
2021-02-22 04:42:53 -0500

    mtsl

---
+++ mergeTwoSortedLists.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        cur = ListNode(0)
+        dummy = cur
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
