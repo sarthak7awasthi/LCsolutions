305e276e347e1a6fea772664c0c4a76da59060e4
sarthak7awasthi <61361866+sarthak7awasthi@users.noreply.github.com>
2021-08-08 17:57:53 -0400

    noi updated

--- numberOfIsland.py
+++ numberOfIsland.py
@@ -19,3 +19,33 @@ class Solution:
         self.dfs(grid,i-1,j)
         self.dfs(grid,i,j+1)
         self.dfs(grid,i,j-1)
+        
+#   another approach with dynamic programming(both are somewhat similar)
+
+class Solution:
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+  
+        def area(r,c):
+            
+            if r<0 or r>=len(grid) or c<0 or c>=len(grid[0]) or grid[r][c]=="0":
+                
+                return 0
+       
+            grid[r][c]="0"
+            
+           
+            return (1 +area(r+1, c)+ area(r-1, c)+area(r, c-1) + area(r, c+1))
+        
+        l=[area(r, c)
+                   for r in range(len(grid))
+                   for c in range(len(grid[0]))]
+
+        counter=0
+        for i in l:
+            if i!=0:
+                
+                counter+=1
+                
+        
+        return counter
