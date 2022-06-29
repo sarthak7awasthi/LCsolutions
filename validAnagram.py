4a975c5966bcbeb5ccb135dc7baf491cc0afcf9d
sarthak7awasthi <61361866+sarthak7awasthi@users.noreply.github.com>
2022-06-29 10:47:03 -0400

    using single dictionary

--- validAnagram.py
+++ validAnagram.py
@@ -19,3 +19,22 @@ class Solution:
                 return False
         else:
             return False
+# Using only 1 dictionary
+#         def isAnagram(self, s: str, t: str) -> bool:
+    #         if len(s)!=len(t):
+    #             return False
+    #         d={}
+    #         for i,j in zip(s,t):
+    #             if d.get(i):
+    #                 d[i]+=1
+    #             else:
+    #                 d[i]=1
+    #             if d.get(j):
+    #                 d[j]-=1
+    #             else:
+    #                 d[j]=-1
+
+    #         for i in d:
+    #             if d[i]!=0:
+    #                 return False
+    #         return True
